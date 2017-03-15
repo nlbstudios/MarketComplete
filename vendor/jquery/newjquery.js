6af169cdec5c4175ddb8e54cf7c671f03e2379fd
NLBSWebDev <webdesign@nlbstudios.com>
2017-03-15 11:02:08 -0400

    First Commit

---
+++ vendor/jquery/newjquery.js
@@ -0,0 +1,12 @@
+// $('.portfolio-item1').on({
+//   'click' : function(){
+//     $('.mainport').attr('src','img/portfolio/portfolio1.png');
+//   }
+// });
+
+// 
+// if ($(window).width() <= 768){
+//   $('.logo').attr('src','img/logos/logomobile.png');
+// } else{
+//   $('.logo').attr('src','img/logos/logocolorchanged2.png');
+// };
